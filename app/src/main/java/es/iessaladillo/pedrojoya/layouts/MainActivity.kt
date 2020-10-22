14664d43dc6669f6d950c6ba331e184714d622b1
pedrojoya <pedrojoya@gmail.com>
2020-10-22 09:43:44 +0200

    Initial commit

---
+++ app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
@@ -0,0 +1,13 @@
+package es.iessaladillo.pedrojoya.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: USAR UNO DE LOS LAYOUTS DEFINIDOS PARA PROBARLO.
+        setContentView(R.layout.linearlayout_activity)
+    }
+}
